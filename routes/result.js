be6fc3e2dff74701f7dc05837c10c71777b9baa5
Alfonso García-Miñaur <Alfonso@Alfonso-GMS.local>
2019-09-18 11:06:51 +0200

    new back

--- routes/result.js
+++ routes/result.js
@@ -1,8 +1,59 @@
+//setup for the project
 const express = require("express");
 const router = express.Router();
+//Amadeus api setup
+const Amadeus = require('amadeus');
+var amadeus = new Amadeus({
+  clientId: 'zOEKFP0AAfN78XN6tbqMDZE2DA3VbuoT',
+  clientSecret: 'ymwDkjLZzeXuRLeA'
+});
+// Axios setup
+const axios=require("axios");
+
+//YELP API SETUP
+const yelp=require("yelp-fusion");
+const apiKey = 'I3MQ56BQ3r0ixQf-BawisZbcp25tfJcvdqIaful1PxX2MNLAOeSZfghR4XyHvKfixnmwaSB6IEuna0bNecNU0L9oZkmjm91xAJy2ZECDnhS_eGU6LxsNKxg-lxiBXXYx';
+const client = yelp.client(apiKey);
+
+
+
+
 
 router.get("/", (req, res) => {
-  res.render("03-result.hbs");
+ //here we get the input of the user (city and days)
+  let city= req.query.city;
+  let days= req.query.days;
+  let itinerary=[];
+  for (let i=1;i<=days;i++){
+   itinerary.push(i);
+  }
+  console.log(city, days)
+  
+  //Get hotels based on a city!!
+  const searchHotel = {
+    sort: "rating",
+    categories: "hotels",
+    location:city,
+  };
+  client.search(searchHotel).then(response => {
+    const firstResult = response.jsonBody.businesses.slice(0,4);
+    let ordered=firstResult.sort(function(a, b) {
+      return parseFloat(b.review_count)-parseFloat(a.review_count);
+  });
+    const prettyJson = JSON.stringify(ordered, null, 4);
+    
+    //console.log("Empieza aqui",prettyJson);
+    console.log("acaba aqui")
+    res.render("result.hbs",{city:city, ordered:ordered[0]});
+  }).catch(e => {
+    console.log(e);
+  });
+  // The places to go based on the hotel coordinates 
+  
+    
+    //yelp api search request based on hotel coordinates 
+    
+  
 });
 
 module.exports = router;
--- routes/search.js
+++ routes/search.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.render("02-search.hbs");
+const Amadeus = require('amadeus');
+const axios=require("axios");
+const API_KEY = '0_nV6ChR3EFhFwwsfs_9JqsBFCkYvf0U17qJXbIazY6sMAy26HK39FklbaJkHhrAsZ_E20BAz3mVY9-6c4vPih5NefgJTUDhynU4wADCt6rphCxm8NTCpA-FoAaBXXYx';
+var amadeus = new Amadeus({
+  clientId: 'zOEKFP0AAfN78XN6tbqMDZE2DA3VbuoT',
+  clientSecret: 'ymwDkjLZzeXuRLeA'
 });
+router.get('/', (req, res, next) => {
+  axios.get("https://api.yelp.com/v3/businesses/search?term=delis&latitude=37.786882&longitude=-122.399972")
+  res.render("search")
+});
+
 
 module.exports = router;
