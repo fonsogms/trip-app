8694df613e7c679d6f082e01eac7bed0b72db7bc
Marco Accardi <contact.marco.accardi@gmail.com>
2019-09-19 09:37:32 +0200

    stable version

--- public/javascripts/commands.js
+++ public/javascripts/commands.js
@@ -8,7 +8,7 @@ const searchInterval = setInterval(() => {
   let command = document.getElementById("recognised").innerText;
   let str = command.split(" ");
 
-  days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "weekend", "week-end"];
   // cities = ["berlin", "london", "rome", "paris", "lisbon", "vienna", "venice"];
   cities = [];
   axios
@@ -43,7 +43,12 @@ const searchInterval = setInterval(() => {
       if (day.length && city.length) {
         console.log("DAY :", day);
         console.log("CITY :", city);
-        document.location.href = `/result?city=${city}&days=${day}`;
+        if (day === "weekend") {
+          let day = 2;
+          document.location.href = `/result?city=${city}&days=${day}`;
+        } else {
+          document.location.href = `/result?city=${city}&days=${day}`;
+        }
         // clearTimeout(searchTime);
       }
       // }, 5000);
@@ -60,54 +65,48 @@ const searchInterval = setInterval(() => {
 // RESULT -> BACK TO SEARCH
 setInterval(() => {
   // setTimeout(() => {
-    let command = document.getElementById("recognised").innerText;
-    let str = command.split(" ");
-    if (
-      (str.includes("destination") ||
-        str.includes("plan") ||
-        str.includes("city")) &&
-      str.includes("change")
-    ) {
-      document.location.href = "/search";
-    }
+  let command = document.getElementById("recognised").innerText;
+  let str = command.split(" ");
+  if (
+    (str.includes("destination") ||
+      str.includes("plan") ||
+      str.includes("city")) &&
+    str.includes("change")
+  ) {
+    document.location.href = "/search";
+  }
   // }, 3000);
 }, 1000);
 
 // LOGOUT
 setInterval(() => {
   // setTimeout(() => {
-    let command = document.getElementById("recognised").innerText;
-    let str = command.split(" ");
-    if (
-      str.includes("logout") ||
-      (str.includes("log") && str.includes("out"))
-    ) {
-      document.location.href = "/";
-    }
-    // console.log(str);
+  let command = document.getElementById("recognised").innerText;
+  let str = command.split(" ");
+  if (str.includes("logout") || (str.includes("log") && str.includes("out"))) {
+    document.location.href = "/";
+  }
+  // console.log(str);
   // }, 3000);
 }, 1000);
 
 // LOGIN
 setInterval(() => {
   // setTimeout(() => {
-    let command = document.getElementById("recognised").innerText;
-    let str = command.split(" ");
-    if ((str.includes("log") && str.includes("in")) || str.includes("login")) {
-      document.querySelector(".log-in").click();
-    }
-    if (
-      (str.includes("login") || (str.includes("log") && str.includes("in"))) &&
-      str.includes("facebook")
-    ) {
-      document.querySelector(".log-fb").click();
-    }
-    if (
-      (str.includes("sign") && str.includes("up")) ||
-      str.includes("signup")
-    ) {
-      document.querySelector(".sign-up").click();
-    }
-    // console.log(str);
+  let command = document.getElementById("recognised").innerText;
+  let str = command.split(" ");
+  if ((str.includes("log") && str.includes("in")) || str.includes("login")) {
+    document.querySelector(".log-in").click();
+  }
+  if (
+    (str.includes("login") || (str.includes("log") && str.includes("in"))) &&
+    str.includes("facebook")
+  ) {
+    document.querySelector(".log-fb").click();
+  }
+  if ((str.includes("sign") && str.includes("up")) || str.includes("signup")) {
+    document.querySelector(".sign-up").click();
+  }
+  // console.log(str);
   // }, 3000);
 }, 1000);
